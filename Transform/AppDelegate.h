9b3c23615085b40db185ca26dcc1f0a56ecd5180
codeliu6572 <codeliu6572@163.com>
2016-03-15 15:06:35 +0800

    Initial Commit

---
+++ Transform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Transform
+//
+//  Created by 刘浩浩 on 16/3/15.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
